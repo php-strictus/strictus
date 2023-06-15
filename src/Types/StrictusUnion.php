2d83328c686767b1d60ad794655323d1bb0e6c3f
Thai Nguyen Hung <hungthai1401.it@gmail.com>
2023-06-15 17:54:08 +0700

    fix: declare type of instance as `class-string` to solve PHPStan

--- src/Types/StrictusUnion.php
+++ src/Types/StrictusUnion.php
@@ -179,6 +179,7 @@ final class StrictusUnion implements StrictusTypeInterface
             return;
         }
 
+        /** @var class-string $instance */
         $this->instances = [
             $this->type->name => $instance,
         ];
